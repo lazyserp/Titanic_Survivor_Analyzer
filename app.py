1230296bfd694f0d7fd79b8b932d43ca910fc303
Aman <Amanjha434@gmail.com>
2025-08-11 01:00:59 +0530

    added buttons

--- app.py
+++ app.py
@@ -7,13 +7,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html.j2")
 
+import pandas
+df = pandas.read_csv('./train.csv')
+
 @app.route("/preview")
 def preview():
-    import pandas
-    
-    df = pandas.read_csv('./train.csv')
-    first_10_rwos = df.head(10).to_html(classes="table table-striped", index=False)
+    first_10_rwos = df.head(10).to_html(classes="table table-striped",bold_rows=True, index=False)
     return first_10_rwos
+
+@app.route("/mean")
+def mean():
+    mean_of_age = df["Age"].mean()
+    return f"<p>Mean Age: {mean_of_age:.2f}</p>"
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
